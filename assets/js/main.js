41b4da031bd879dcbc0c9426247c0e1b1ba22484
Pedro Baraldi Sá <125573228+pedrobaraldi@users.noreply.github.com>
2025-03-11 18:26:41 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,105 @@
+/*==================== CLOCK ====================*/
+const hour = document.getElementById('clock-hour'),
+    minutes = document.getElementById('clock-minutes'),
+    seconds = document.getElementById('clock-seconds')
+
+const clock = () => {
+    let date = new Date()
+
+    let hh = date.getHours() * 30,
+        mm = date.getMinutes() * 6,
+        ss = date.getSeconds() * 6
+
+
+    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
+    minutes.style.transform = `rotateZ(${mm}deg)`
+    seconds.style.transform = `rotateZ(${ss}deg)`
+}
+setInterval(clock, 1000)
+
+const textHour = document.getElementById('text-hour'),
+    textMinutes = document.getElementById('text-minutes'),
+    textAmPm = document.getElementById('text-ampm'),
+    dateWeek = document.getElementById('date-day-week'),
+    dateDay = document.getElementById('date-day'),
+    dateMonth = document.getElementById('date-month'),
+    dateYear = document.getElementById('date-year')
+
+const clockText = () => {
+    let date = new Date()
+
+    let hh = date.getHours(),
+        ampm,
+        mm = date.getMinutes(),
+        day = date.getDate(),
+        dayweek = date.getDay(),
+        month = date.getMonth(),
+        year = date.getFullYear()
+
+    // // We change the hours from 24 to 12 hours and establish whether it is AM or PM
+    // // if (hh >= 12) {
+    // //     hh = hh - 12
+    // //     ampm = 'PM'
+    // // } else {
+    // //     ampm = 'AM'
+    // // }
+
+    // //  We detect when it's 0 AM and transform to 12 AM
+    // // if (hh == 0) { hh = 12 }
+
+
+    if (hh < 10) { hh = `0${hh}` }
+
+
+    textHour.innerHTML = `${hh}:`
+
+
+    if (mm < 10) { mm = `0${mm}` }
+
+
+    textMinutes.innerHTML = mm
+
+    // Show am or pm
+    // textAmPm.innerHTML = ampm
+
+    let week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+
+
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
+    dateDay.innerHTML = day
+    dateWeek.innerHTML = `${week[dayweek]}`
+    dateMonth.innerHTML = `${months[month]},`
+    dateYear.innerHTML = year
+}
+setInterval(clockText, 1000)
+
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
+
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
+
+if (selectedTheme) {
+
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
